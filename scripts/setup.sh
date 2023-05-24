3d4fc5d273a9a5164efd39850a655c20e326fb6e
Abiram98 <abiram98@gmail.com>
2023-05-24 10:15:38 +0200

    req file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,13 @@
 #!bin/bash
 
 
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip3 install sparqlwrapper torch-geometric networkx pandas configparser matplotlib torchsummary
-pip3 install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.0.0+cpu.html
+#pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+#pip3 install sparqlwrapper torch-geometric networkx pandas configparser matplotlib torchsummary
+#pip3 install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.0.0+cpu.html
 
-pip install  dgl -f https://data.dgl.ai/wheels/repo.html
-pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
-pip install matplotlib
-pip install pyvis
+#pip install  dgl -f https://data.dgl.ai/wheels/repo.html
+#pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
+#pip install matplotlib
+#pip install pyvis
+(cd .. && pip3 install -r requirements.txt )
 pip3 install -e ../
