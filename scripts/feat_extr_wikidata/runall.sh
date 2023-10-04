1e68da387b100345834c0cb217b1684ff854dd8e
Abiram98 <abiram98@gmail.com>
2023-10-04 11:44:06 +0000

    pred co extract

--- scripts/feat_extr_wikidata/runall.sh
+++ scripts/feat_extr_wikidata/runall.sh
@@ -4,24 +4,25 @@ output_dir=/PlanRGCN/extracted_features_wd
 pred_file=predicates.json
 # mkdir -p /PlanRGCN/extracted_features_wd/predicate/batches
 
-: 'python3 -m feature_extraction.predicates.pred_util $task \
+python3 -m feature_extraction.predicates.pred_util $task \
         -e $url \
         --output_dir $output_dir \
         --pred_file $pred_file
-'
+
 
 task=extract-co-predicates
 input_dir=/PlanRGCN/extracted_features_wd
 output_dir=/PlanRGCN/extracted_features_wd/predicate
 batch_start=1
 batch_end=10000000
-: 'python3 -m feature_extraction.predicates.pred_util $task -e $url \
+python3 -m feature_extraction.predicates.pred_util $task -e $url \
         --input_dir $input_dir \
         --output_dir $output_dir \
         --pred_file $pred_file \
         --batch_start $batch_start \
         --batch_end $batch_end
-
+mkdir -p /PlanRGCN/extracted_features_wd/predicate/pred_co
+mv /PlanRGCN/extracted_features_wd/predicate/batch_response/* /PlanRGCN/extracted_features_wd/predicate/pred_co
 
 task=extract-predicates-stat
 input_dir=/PlanRGCN/extracted_features_wd/predicate
@@ -34,16 +35,17 @@ python3 -m feature_extraction.predicates.pred_stat_feat $task -e $url \
         --pred_file $pred_file \
         --batch_start $batch_start \
         --batch_end $batch_end
-'
 
-: '
+
+
 task=extract-entity-query-log
 queries=/SPARQLBench/wdbench/bgp_opts.tsv
 outputPath=/PlanRGCN/extracted_features_wd/entities/entities_in_wikidata.json
 mkdir -p /PlanRGCN/extracted_features_wd/entities/
 mvn package -f /PlanRGCN/qpe/pom.xml
 java -jar /PlanRGCN/qpe/target/qpe-1.0-SNAPSHOT.jar $task $queries $outputPath
-'
+
+
 task=extract-entity-stat
 output_dir=/PlanRGCN/extracted_features_wd/entities
 pred_file=/PlanRGCN/extracted_features_wd/entities/entities_in_wikidata.json
@@ -53,3 +55,4 @@ batch_end=10000000
 
 
 python3 -m feature_extraction.entity.entity_util $task -e $url --output_dir $output_dir --ent_file $pred_file --batch_start $batch_start --batch_end $batch_end
+
