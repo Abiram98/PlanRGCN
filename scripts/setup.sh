32349a0461fcb9d45f17bf2a59051a32fa3a1b9c
Abiram98 <abiram98@gmail.com>
2023-12-13 05:02:38 +0000

    updated setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,7 +24,8 @@ pip3 install -U "ray[data,train,tune,serve]"
 apt-get -y install maven
 apt-get -y install openjdk-17-jdk openjdk-17-jre
 #mvn exec:java -f "/PlanRGCN/qpe/pom.xml"
-
+mvn package -f "/PlanRGCN/qpe/pom.xml"
+mvn install:install-file -Dfile=/PlanRGCN/qpe/target/qpe-1.0-SNAPSHOT.jar -DpomFile=/PlanRGCN/qpe/pom.xml
 
 apt-get install graphviz graphviz-dev -y
 pip install pygraphviz
