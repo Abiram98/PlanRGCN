5d3962459f0be0f74feaee32bbb6fb24363f91ac
Abiram98 <abiram98@gmail.com>
2024-02-13 12:16:26 +0000

    DPedia feat_rep

--- feature_representation/feat_rep/config.py
+++ feature_representation/feat_rep/config.py
@@ -1,6 +1,6 @@
 config = {
-        'save_dir' : '/data/data/dbpedia2016/predicate/pred_co',
-        'pred_co_pair_dir' : '/data/data/dbpedia2016/predicate/pred_co/batch_response/',
+        'save_dir' : '/PlanRGCN/data/dbpedia2016/predicate/pred_co',
+        'pred_co_pair_dir' : '/PlanRGCN/data/dbpedia2016/predicate/batch_response/',
 }
 config["comm"] = f"{config['save_dir']}/communities_louvain.pickle"
 config["p2i"] = f"{config['save_dir']}/pred2index_louvain.pickle"
