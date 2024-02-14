383760dd44fde7e5ec157826c4852f703c72034e
Abiram98 <abiram98@gmail.com>
2024-02-14 14:33:19 +0000

    restructure

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,27 +13,27 @@ pip3 install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
 pip3 install matplotlib
 pip3 install pyvis
 pip3 install pydot
-(cd .. && pip3 install -r requirements.txt )
+pip3 install -r /PlanRGCN/requirements.txt
 pip3 install notebook
 #pip3 install -e ../
-pip3 install -e feature_extraction
-pip3 install -e graph_construction
-pip3 install -e dgl_classifier 
+pip3 install -e PlanRGCN/feature_extraction
+pip3 install -e PlanRGCN/graph_construction
+pip3 install -e PlanRGCN/dgl_classifier 
 pip3 install -U "ray[data,train,tune,serve]"
 #java installation
 apt-get -y install maven
 apt-get -y install openjdk-17-jdk openjdk-17-jre
 #mvn exec:java -f "/PlanRGCN/qpe/pom.xml"
-mvn package -f "/PlanRGCN/qpe/pom.xml"
+mvn package -f "/PlanRGCN/PlanRGCN/qpe/pom.xml"
 mvn install:install-file -Dfile=/PlanRGCN/qpe/target/qpe-1.0-SNAPSHOT.jar -DpomFile=/PlanRGCN/qpe/pom.xml
 
 apt-get install graphviz graphviz-dev -y
 pip install pygraphviz
 
-pip3 install -e feature_representation/
-pip3 install -e trainer
-pip3 install -e sample_checker 
-pip3 install -e utils/
+pip3 install -e PlanRGCN/feature_representation/
+pip3 install -e PlanRGCN/trainer
+pip3 install -e PlanRGCN/sample_checker 
+pip3 install -e PlanRGCN/utils/
 pip3 install -e load_balance/
 
 pip3 install xgboost
--- util.sh
+++
@@ -1,7 +0,0 @@
-compress() {
-tar -czvf "$1".tar.gz "$2"
-}
-
-decompress(){
-tar -xzvf "$1".tar.gz
-}
