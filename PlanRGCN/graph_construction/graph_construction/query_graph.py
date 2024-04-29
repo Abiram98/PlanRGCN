d9324fbc2986b6e1e4efeaa0f766b93eaca60404
Abiram98 <abiram98@gmail.com>
2024-04-29 16:19:32 +0000

    training new time intervals

--- PlanRGCN/graph_construction/graph_construction/query_graph.py
+++ PlanRGCN/graph_construction/graph_construction/query_graph.py
@@ -202,6 +202,20 @@ def snap_lat2onehotv2(lat):
         vec[2] = 1
     return vec
 
+def snap5cls(lat):
+    vec = np.zeros(5)
+    if lat < 0.004:
+        vec[0] = 1
+    elif (0.004 < lat) and (lat <= 1):
+        vec[1] = 1
+    elif (1 < lat) and (lat < 10):
+        vec[2] = 1
+    elif (10 < lat) and (lat < 899):
+        vec[3] = 1
+    elif 899 < lat:
+        vec[4] = 1
+    return vec
+
 def snap_reg(lat):
     return np.array([lat])
 
